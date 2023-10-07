a79189e92e8bf579159c8b3f3f3f05bcaf9e5faf
Dhruv Sharma <dsd71213@gmail.com>
2023-10-08 01:30:23 +0530

    Added Contact Form

--- animation.js
+++ animation.js
@@ -5,3 +5,23 @@ function typing() {
         loop: true
     })
 };
+let nm;
+let m;
+let msg;
+function sendEmail() {
+    nm = document.getElementById("name").value;
+    m = document.getElementById("mail").value;
+    msg = document.getElementById("message").value;
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "dhrruvsharma@outlook.com",
+        Password: "C7ADC34324CC8E2BB3E50849DA3A3322128F",
+        To: 'dsd71213@gmail.com',
+        From: "dhrruvsharma@outlook.com",
+        Subject: "Someone contacted you",
+        Body: "Name: " + nm + "<br> Email: " + m + "<br> Message: " + msg,
+    })
+        .then(function (message) {
+            alert("mail sent successfully")
+        })
+}

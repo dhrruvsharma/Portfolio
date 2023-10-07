3c02b7491e4110aec39916d78b55e43b49ba39ca
Dhruv Sharma <dsd71213@gmail.com>
2023-10-07 19:24:26 +0530

    New Portfolio

---
+++ animation.js
@@ -0,0 +1,7 @@
+function typing() {
+    var typed = new Typed(".input", {
+        strings: ["Dhruv Sharma", "a programmer", "a developer"],
+        typeSpeed: 200,
+        loop: true
+    })
+};
